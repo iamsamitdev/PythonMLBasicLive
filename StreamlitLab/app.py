96931b40111e460cad02dc4a77b22f5bf6166132
Samit Koyom <samitkoyom@gmail.com>
2024-02-05 23:14:24 +0700

    update day 4

---
+++ StreamlitLab/app.py
@@ -0,0 +1,18 @@
+# import streamlit as st
+import streamlit as st
+
+# Title
+st.title('Streamlit Lab')
+
+# Header
+st.header('This is a header')
+
+# body
+st.write('This is a body')
+
+# Input
+st.text_input('Enter your name')
+st.text_area('Enter your message')
+
+# Button
+st.button('Click me')
